f99d3c143566e3d6ccc8bff2a590a8243bc66b18
dibyendutapadar <dibyendutapadar@gmail.com>
2024-08-04 13:23:15 +0530

    Initial commit4

--- src/main.py
+++ src/main.py
@@ -2,6 +2,15 @@ import streamlit as st
 from multiapp import MultiApp
 from apps import home, forecast
 
+
+
+st.set_page_config(
+    page_title="Time Series Forecasting App",
+    page_icon="📈",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
 app = MultiApp()
 
 # Add all your application here
